50b0df5b4427ec543d5bea1c15754ed05bbde87f
Vinay Rathore <90103557+vinay312@users.noreply.github.com>
2026-06-10 17:03:08 +0000

    LeetCode Synchronization - Max Consecutive Ones (C++)

---
+++ problems/0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count =0;
+        int maxCount=0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==1){
+                count++;
+            }
+
+            else {
+               
+                count=0;
+            }
+
+            if(maxCount<count)
+                maxCount=count;
+
+        
+        }
+
+     return maxCount;
+    }
+};
