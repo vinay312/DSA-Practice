73aa2c058f9b7da98216d2a895eb11fe093d8b08
Vinay Rathore <90103557+vinay312@users.noreply.github.com>
2026-08-27 17:36:21 +0000

    LeetCode Synchronization - Binary Search (C++)

--- problems/0704-binary-search/0704-binary-search.cpp
+++ problems/0704-binary-search/0704-binary-search.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        int low=0;
-        int high= nums.size()-1;
 
+    int binarySearchrec(vector<int>& nums, int target, int low, int high){
 
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]== target)
-                return mid;
-            
-            else if(nums[mid]>target)
-                    high= mid-1;
-            else
-                low= mid+1;
+        if(low > high)
+            return -1;
+
+        int mid = (low + high) >> 1;
+
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] > target){
+           return binarySearchrec(nums, target, low, mid-1);
+        
         }
-        return -1;
+        else{
+
+           return binarySearchrec(nums, target, mid+1, high);
+
+        }
+
+     
+    }
+
+    int search(vector<int>& nums, int target) {
+
+        int n = nums.size() - 1;
+
+        return binarySearchrec(nums, target, 0, n);
+     
 
     }
 
