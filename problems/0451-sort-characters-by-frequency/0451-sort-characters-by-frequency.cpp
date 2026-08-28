fba15d31bc843492cc700e11e46ebd04236435f3
Vinay Rathore <90103557+vinay312@users.noreply.github.com>
2026-08-28 14:47:40 +0000

    LeetCode Synchronization - Sort Characters By Frequency (C++)

--- problems/0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ problems/0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -4,24 +4,24 @@ class Solution {
 
     static bool compare(pair<char, int>& a, pair<char, int>& b) {
 
-        // Higher frequency comes first
+      
         if (a.second != b.second)
             return a.second > b.second;
 
-        // If frequencies are same, alphabetical order
+    
         return a.first < b.first;
     }
     public:
         string frequencySort(string s) {
 
-        // Step 1: Count frequencies
+
         int freq[128] = {0};
 
         for (char ch : s) {
             freq[ch]++;
         }
 
-        // Step 2: Store (character, frequency) pairs
+
         vector<pair<char, int>> v;
 
         for (int i = 0; i < 128; i++) {
@@ -33,10 +33,10 @@ class Solution {
             }
         }
 
-        // Step 3: Sort according to comparator
+
         sort(v.begin(), v.end(), compare);
 
-        // Step 4: Build answer
+
         string ans = "";
 
         for (auto p : v) {
