1bb1394ab40c506770f7fadb7768c113248e581b
Vinay Rathore <90103557+vinay312@users.noreply.github.com>
2026-09-04 17:44:03 +0000

    LeetCode Synchronization - Valid Parentheses (C++)

--- problems/0020-valid-parentheses/0020-valid-parentheses.cpp
+++ problems/0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,29 +2,25 @@ class Solution {
 public:
     bool isValid(string s) {
 
-        unordered_map<char, char> mpp ={
-            {')','('} , {'}','{'} , {']', '['}
-        };
+        stack<int> stk;
+        int n = s.size();
 
-        stack<char> sk;
-
-        for(int i = 0; i<s.size(); i++){
-            if(s[i] == '(' || s[i] == '[' || s[i] == '{' )
-                sk.push(s[i]);
-            
+        for(int i = 0; i<n; i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{')
+                stk.push(s[i]);
             else{
-                if(sk.empty())
+                if(stk.empty())
                     return false;
-                
-                if(sk.top() != mpp[s[i]])
+                char ch= stk.top();
+                stk.pop();
+
+                if((s[i]==')' && ch == '(') || (s[i]==']' && ch == '[') || (s[i]=='}' && ch == '{'))
+                    continue;
+                else
                     return false;
 
-                
-                sk.pop();
             }
-
         }
-            return sk.empty();
-        
+        return stk.empty();
     }
 };
