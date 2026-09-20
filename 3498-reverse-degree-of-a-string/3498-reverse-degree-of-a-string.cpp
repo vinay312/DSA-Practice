efc359ecb04b84249648760d9240b718503faf67
Vinay Rathore <90103557+vinay312@users.noreply.github.com>
2026-09-20 15:51:13 +0530

    Time: 2 ms (19.75%), Space: 9.6 MB (95.77%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -5,8 +5,10 @@ public:
         int sum =0;
 
         for(int i =0; i<s.size(); i++){
-            int temp = (26 - (s[i]-'a')) * (i+1);
-            sum= sum + temp;
+            int revVal = (26 - (s[i]-'a'));
+            int indProduct = revVal*(i+1);
+
+            sum= sum + indProduct;
         }
 
         return sum;
